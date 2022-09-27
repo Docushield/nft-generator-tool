fe3de126fcd681ca245102f028a862f84cb0882c
Halton Xu <halton.xu@gmail.com>
2022-09-27 20:59:11 +0800

    update code

--- src/pages/api/generate.js
+++ src/pages/api/generate.js
@@ -5,6 +5,9 @@ import _ from "lodash";
 import fse from "fs-extra";
 import { _fetchFilePath } from "@/utils/helper";
 import axios from "axios";
+import { basename } from "path";
+import ipfsOnlyHash from "ipfs-only-hash";
+import crypto from "crypto"
 async function handle(req, res) {
   const { method, body } = req;
   switch (method) {
@@ -28,8 +31,36 @@ async function handlePost(data, res) {
       return footprint;
     }
   });
+  
+  await _buildOutputfile(footprint);
   const zipfile = await _zipOutputfile(footprint);
   res.status(200).json(zipfile);
+}
+
+async function _buildOutputfile(footprint) {
+  const storageDir = process.env.STORAGE;
+  const jsonDir = `${storageDir}/${footprint}/build/json`;
+  const originalFile = `${jsonDir}/nft-collection-original.json`;
+  let rawdata = fs.readFileSync(originalFile);
+  let jsonData = JSON.parse(rawdata);
+  // console.log(jsonData);
+  let tokenList = jsonData['token-list'];
+  for (let it = 0; it < tokenList.length; it++) {
+    let token = tokenList[it];
+    const cid = await ipfsOnlyHash.of(fs.readFileSync(token.hash));
+    // console.log("cid:",cid);
+    token.content_uri = {
+      scheme:`ipfs://${cid}`,
+      data:cid
+    }
+    const sha256hash = crypto.createHash("sha256").update(fs.readFileSync(token.hash)).digest("hex")
+    const base64hash = Buffer.from(sha256hash).toString('base64')
+    token.hash = base64hash
+  }
+  console.log("tokenList:",tokenList);
+  jsonData["token-list"] = tokenList;
+
+  fs.writeFileSync(`${jsonDir}/nft-collection.json`, JSON.stringify(jsonData, null, 2));
 }
 
 async function _zipOutputfile(footprint) {
@@ -41,6 +72,7 @@ async function _zipOutputfile(footprint) {
   return new Promise((resolve, reject) => {
     const JSzipHdl = new JSZip();
     paths.forEach(file => {
+      if (basename(file) == "nft-collection-original.json") return;
       const relativeFile = _.split(file,footprint)[1];
       // console.log("relativeFile:", relativeFile);
       if (relativeFile) {
